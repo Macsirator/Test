bfca0b5826a2110d4b1356cdddfe1693ef67f932
Macsirator <minecraftmacsimr@gmail.com>
2021-01-16 16:33:26 +0900

    Test

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication.data.MobileDark2;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void Osu(View view) {
+        Intent intent = new Intent(this, MobileDark2.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/data/MobileDark2.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class MobileDark2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_dark2);
+    }
+}
